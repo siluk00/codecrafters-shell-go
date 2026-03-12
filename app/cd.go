b6c17fc6286bf1b8b959748426c7992799c3685c
siluk00 <sil-uk@hotmail.com>
2026-03-11 23:10:10 -0300

    codecrafters submit [skip ci]

--- app/cd.go
+++ app/cd.go
@@ -3,31 +3,41 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func (c *Cmd) cdCommand(args []string) {
+	var path string
 	if len(args) > 1 {
 		fmt.Println("cd: too many arguments")
 	}
 	if len(args) == 0 {
 		return
 	}
-	if args[0][0] == '/' {
-		fileInfo, err := os.Stat(args[0])
 
-		if err != nil {
-			if os.IsNotExist(err) {
-				fmt.Printf("cd: %s: No such file or directory\n", args[0])
-			}
-			return
-		}
-
-		if fileInfo.IsDir() {
-			c.curDir = args[0]
-			//fmt.Printf("%s\n", args[0])
-		}
-	} else {
+	switch args[0][0] {
+	case '/':
+		path = args[0]
+	case '.':
+		path = filepath.Join(c.curDir, args[0])
+	default:
 		fmt.Printf("cd: %s: No such file or directory\n", args[0])
-
+		return
 	}
+
+	fileInfo, err := os.Stat(path)
+
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Printf("cd: %s: No such file or directory\n", args[0])
+		}
+		return
+	}
+
+	if fileInfo.IsDir() {
+		c.curDir = args[0]
+		//fmt.Printf("%s\n", args[0])
+	}
+
+	fmt.Printf("cd: %s: No such file or directory\n", args[0])
 }
