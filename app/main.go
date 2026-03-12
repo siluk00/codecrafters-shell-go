2ae5fc0fec407c7c2f0bbc6fbc77039b56f42952
siluk00 <sil-uk@hotmail.com>
2026-03-11 22:35:58 -0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -13,7 +13,7 @@ import (
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Print
 
-var builtinCommands = []string{"echo", "exit", "type"}
+var builtinCommands = []string{"echo", "exit", "type", "pwd"}
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
@@ -36,6 +36,8 @@ func main() {
 			echoCommand(args)
 		case "type":
 			typeCommand(args)
+		case "pwd":
+			pwdCommand()
 		default:
 			cmd := exec.Command(command, args...)
 			cmd.Stdout = os.Stdout
